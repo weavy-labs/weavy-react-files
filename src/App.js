6395baf50efd091d304f467d3634d235377bc894
nmai601 <mai@weavy.com>
2021-09-09 14:50:26 -0700

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,27 @@
-import logo from './logo.svg';
+import { Component } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+import Weavy from './weavy/Weavy';
+import WeavyChat from './weavy/WeavyChat';
 
-export default App;
+export default class App extends Component {
+  async getJwt() {
+    return 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJ0ZXN0IiwiaWF0IjoxNjA1MDM1MTY2LCJleHAiOjE2MzY1NzExNzYsInN1YiI6InRlc3RlciIsImNsaWVudF9pZCI6ImNsaWVudGlkIiwiZW1haWwiOiJ0ZXN0ZXJAZW1haWwuY29tIiwidXNlcm5hbWUiOiJ0ZXN0ZXIifQ.MhPMARKc1UaFtcjnvDmO-Z8fFmxdp4EE-Bz-0H3-doQ';
+  }
+
+  render() {
+    return (
+      <Weavy jwt={this.getJwt}>
+        <div className="App">
+          <WeavyChat
+            spaceKey="react-space"
+            spaceName="React Space"
+            appKey="react-chat"
+            appName="React Chat"
+            appType="messenger"
+            />
+        </div>
+      </Weavy>
+    );
+  }
+}
